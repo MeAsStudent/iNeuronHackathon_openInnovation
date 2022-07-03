66863980689be3ea7cb50bb399a14813cfd54c01
MeAsStudent <95217981+MeAsStudent@users.noreply.github.com>
2022-07-03 06:02:30 +0530

    Update App.js
    updated code

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NativeBaseProvider, Box , Center, Image, Stagger, IconButton, HStack, useDisclose, Icon, VStack, Thumbnail} from "native-base";
+import React, { useState }  from 'react'
+import { Alert, StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import { NativeBaseProvider, Box , Center, Image, Stagger, IconButton, HStack, useDisclose, Icon, VStack, Avatar, ScrollView} from "native-base";
 import Home from './src/Screens/Home';
 import Login from './src/Screens/Login';
 import {MaterialCommunityIcons} from '@expo/vector-icons';
@@ -8,40 +9,36 @@ import image from './assets/images/hint.jpg';
 
 const App = () => {
 
+  const [title, setTitle] = useState("The elemet is ntg");
+
   const {
     isOpen,
     onToggle
   } = useDisclose();
 
+  const getlogin = () => {
+      console.log("hi")
+      setTitle("hii")
+  }
 
      return (
       <NativeBaseProvider>
-             <Box  p="12" bg="primary.500" rounded="xl" marginTop={20} marginBottom={40} marginLeft={35} marginRight={35} _text={{
+             <Box  p="12" bg="primary.500" rounded="xl" marginTop={"20%"} marginLeft={"15%"} marginRight={"15%"} marginBottom={"60%"}  _text={{
       fontSize: "md",
       fontWeight: "medium",
       color: "warmGray.50",
       textAlign: "center",
-      height: "100%"
-    }}>
-        This is a Box with Linear Gradient
+    }}><TextInput value = {title}></TextInput>
         </Box>
       
       <Box _text={{
           flexDirection: "row",
           justifyContent: "space-evenly"
       }}>
-        {/* <Center >
-              <Image size={50} resizeMode={"contain"} borderRadius={100} display={"flex"} source={{
-              uri: "https://wallpaperaccess.com/full/317501.jpg"
-            }} alt="Alternate Text" />
-                    <Image size={50} resizeMode={"contain"} borderRadius={100} display={"flex"} source={{
-              uri: "https://wallpaperaccess.com/full/317501.jpg"
-            }} alt="Alternate Text" />       
-       </Center>; */}
 
        <Box>
       <Box alignItems="center" _text={{
-        marginTop: 100,
+        marginTop: 20,
         padding: 20 }}>
         <Stagger visible={isOpen} initial={{
         opacity: 0,
@@ -72,78 +69,127 @@ const App = () => {
         }
       }}>
 
-       <VStack space={4} alignItems="center" justifyContent="space-evenly" marginBottom={60} >
-       <HStack space={10} justifyContent="space-evenly" >
-            
-            <IconButton mb="4" variant="solid" bg="pink.500" colorScheme="pink" borderRadius="full" icon={<Icon size="12" name="location-pin" _dark={{
-                color: "white"
-              }} color="white" />}  />
-            <IconButton mb="4" variant="solid" bg="indigo.500" colorScheme="indigo" borderRadius="full" icon={<Icon size="12" name="image" _dark={{
-                color: "warmGray.50"
-              }} color="warmGray.50" />} />      
-            <IconButton mb="4" variant="solid" bg="indigo.500" colorScheme="indigo" borderRadius="full" icon={<Icon size="12" name="location-pin" _dark={{
-                color: "warmGray.50"
-              }} color="warmGray.50" />} />   
-                  
+        
+          <VStack space={10} alignItems="center" marginLeft={5} marginRight={5} justifyContent="space-evenly" marginBottom={"20%"}  >
+            <HStack justifyContent="space-evenly" >
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          <HStack space={10}  justifyContent='space-evenly'>
+     
+      <TouchableOpacity 
+          onPress = { () => getlogin() }> 
+          <Avatar bg="green.500" size="lg"  rounded='sm'  _image={{rounded: 'sm'}} source={{
+          uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+        }}>
+            AJ
+          </Avatar>
+      </TouchableOpacity>
+
+
+      
+      <Avatar bg="cyan.500" size="lg"  rounded='sm'  _image={{rounded: 'sm'}} source={{
+      uri: "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+    }}>
+        TE
+      </Avatar>
+      <Avatar bg="indigo.500" size="lg" rounded='full'  _image={{rounded: 'xs'}} source={{
+      uri: "https://images.unsplash.com/photo-1614289371518-722f2615943d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+    }}>
+        JB
+      </Avatar>
+      <Avatar bg="amber.500" size="lg" rounded='sm'  _image={{rounded: 'sm'}} source={{
+      uri: "https://images.unsplash.com/photo-1607746882042-944635dfe10e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+    }}>
+        TS
+      </Avatar>
+
+      <Avatar bg="green.500" size="lg" rounded='sm'  _image={{rounded: 'sm'}} source={{
+      uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+    }}>
+        AJ
+      </Avatar>
+      <Avatar bg="cyan.500" size="lg" rounded='sm'  _image={{rounded: 'sm'}} source={{
+      uri: "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+    }}>
+        TE
+      </Avatar>
+      <Avatar bg="indigo.500" size="lg" rounded='sm'  _image={{rounded: 'sm'}} source={{
+      uri: "https://images.unsplash.com/photo-1614289371518-722f2615943d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+    }}>
+        JB
+      </Avatar>
+      <Avatar bg="amber.500" size="lg" rounded='sm'  _image={{rounded: 'sm'}} source={{
+      uri: "https://images.unsplash.com/photo-1607746882042-944635dfe10e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+    }}>
+        TS
+      </Avatar>
+      
             {/* <IconButton mb="4" variant="solid" bg="indigo.500" colorScheme="indigo" borderRadius="full" icon={<Icon size="9" name="location-pin" _dark={{
                 color: "warmGray.50"
               }} color="warmGray.50" />} />   */}
-                </HStack>
-          <HStack space={10} justifyContent="center">
-            
-      <IconButton mb="4" variant="solid" bg="indigo.500" colorScheme="indigo" borderRadius="full" icon={<Icon size="12" name="location-pin" _dark={{
-          color: "warmGray.50"
-        }} color="warmGray.50" />} />
-      <IconButton mb="4" variant="solid" bg="indigo.500" colorScheme="indigo" borderRadius="full" icon={<Icon size="12" name="location-pin" _dark={{
-          color: "warmGray.50"
-        }} color="warmGray.50" />} />      
-      <IconButton mb="4" variant="solid" bg="indigo.500" colorScheme="indigo" borderRadius="full" icon={<Icon size="12" name="location-pin" _dark={{
-          color: "warmGray.50"
-        }} color="warmGray.50" />} />  
-            
-            {/* <IconButton mb="4" variant="solid" bg="indigo.500" colorScheme="indigo" borderRadius="full" icon={<Icon size="7" name="location-pin" _dark={{
-                color: "warmGray.50"
-              }} color="warmGray.50" />} />    */}
-          </HStack>
-          <HStack space={10}  justifyContent="center">
-            
-            <IconButton mb="4" variant="solid" bg="indigo.500" colorScheme="indigo" borderRadius="full" icon={<Icon size="12" name="location-pin" _dark={{
-                color: "warmGray.50"
-              }} color="warmGray.50" />} />
-            <IconButton mb="4" variant="solid" bg="indigo.500" colorScheme="indigo" borderRadius="full" icon={<Icon size="12" name="location-pin" _dark={{
-                color: "warmGray.50"
-              }} color="warmGray.50" />} />      
-            <IconButton mb="4" variant="solid" bg="indigo.500" colorScheme="indigo" borderRadius="full" icon={<Icon size="12" name="location-pin" _dark={{
-                color: "warmGray.50"
-              }} color="warmGray.50" />} />   
-                  
-            {/* <IconButton mb="4" variant="solid" bg="indigo.500" colorScheme="indigo" borderRadius="full" icon={<Icon size="7" name="location-pin" _dark={{
+            </HStack>
+            </ScrollView>
+            </HStack>        
+          
+            <HStack justifyContent="space-evenly" >
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          <HStack space={10} justifyContent='space-evenly'>
+       <Avatar bg="green.500" size="lg" source={{
+      uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+    }}>
+        AJ
+      </Avatar>
+      <Avatar bg="cyan.500" size="lg" source={{
+      uri: "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+    }}>
+        TE
+      </Avatar>
+      <Avatar bg="indigo.500" size="lg" source={{
+      uri: "https://images.unsplash.com/photo-1614289371518-722f2615943d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+    }}>
+        JB
+      </Avatar>
+      <Avatar bg="amber.500" size="lg" source={{
+      uri: "https://images.unsplash.com/photo-1607746882042-944635dfe10e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+    }}>
+        TS
+      </Avatar>
+
+      <Avatar bg="green.500" size="lg" source={{
+      uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+    }}>
+        AJ
+      </Avatar>
+      <Avatar bg="cyan.500" size="lg" source={{
+      uri: "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+    }}>
+        TE
+      </Avatar>
+      <Avatar bg="indigo.500" size="lg" source={{
+      uri: "https://images.unsplash.com/photo-1614289371518-722f2615943d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+    }}>
+        JB
+      </Avatar>
+      <Avatar bg="amber.500" size="lg" source={{
+      uri: "https://images.unsplash.com/photo-1607746882042-944635dfe10e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+    }}>
+        TS
+      </Avatar>
+      
+            {/* <IconButton mb="4" variant="solid" bg="indigo.500" colorScheme="indigo" borderRadius="full" icon={<Icon size="9" name="location-pin" _dark={{
                 color: "warmGray.50"
               }} color="warmGray.50" />} />   */}
-                </HStack>
-          {/* <HStack space={7} justifyContent="center">
-            
-      <IconButton mb="4" variant="solid" bg="indigo.500" colorScheme="indigo" borderRadius="full" icon={<Icon size="7" name="location-pin" _dark={{
-          color: "warmGray.50"
-        }} color="warmGray.50" />} />
-      <IconButton mb="4" variant="solid" bg="indigo.500" colorScheme="indigo" borderRadius="full" icon={<Icon size="7" name="location-pin" _dark={{
-          color: "warmGray.50"
-        }} color="warmGray.50" />} />      
-      <IconButton mb="4" variant="solid" bg="indigo.500" colorScheme="indigo" borderRadius="full" icon={<Icon size="7" name="location-pin" _dark={{
-          color: "warmGray.50"
-        }} color="warmGray.50" />} />  
-            
-            <IconButton mb="4" variant="solid" bg="indigo.500" colorScheme="indigo" borderRadius="full" icon={<Icon size="7" name="location-pin" _dark={{
-                color: "warmGray.50"
-              }} color="warmGray.50" />} />   
-          </HStack> */}
+            </HStack>
+            </ScrollView>
+            </HStack>        
+          
+
 
         </VStack> 
 
         </Stagger>
       </Box>
-      <HStack justifyContent="center">
-        <IconButton variant="solid" borderRadius="full" size="lg" onPress={onToggle} bg="cyan.400" icon={<Icon as={MaterialCommunityIcons} size="6" name="dots-horizontal" color="warmGray.50" _dark={{
+      <HStack justifyContent="center" bottom={-50}>
+        <IconButton variant="solid" borderRadius="full" size="lg" onPress={onToggle} bg="cyan.400" icon={<Icon as={MaterialCommunityIcons} size="6" name="dots-horizontal" bottom={-50} color="warmGray.50" _dark={{
         color: "warmGray.50"
       }} />} />
       </HStack>
